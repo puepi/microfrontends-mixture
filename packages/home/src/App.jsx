06a687fbd8dd22547b2c5cc161c378c7c7d0e292
karoud <pmandel0001@yahoo.fr>
2023-02-24 23:47:59 +0100

    configured the server

--- packages/home/src/App.jsx
+++ packages/home/src/App.jsx
@@ -7,7 +7,7 @@ import Footer from "./Footer";
 
 const App = () => (
   <div className="text-3xl mx-auto max-w-6xl">
-    <Header app={{name:"Home"}}/>
+    <Header/>
       <div className="my-10">
         Page d'accueil affichant les liens vers les différents modules du Sigif.<br/>
         <ul className="text-center p-2">
@@ -15,9 +15,13 @@ const App = () => (
           <li><a href="">Abattages</a></li>
           <li><a href="">Titres forestiers</a></li>
           <li><a href="">Opérations parcs</a></li>
+          <li>....</li>
+          <li>....</li>
+          <li>....</li>
         </ul>
         Chaque module est une application à part entière developpée éventuellement par une seule équipe.<br/>
         Cette page d'accueil (micro app) rend disponible aux autres l'en-tête et  le pied de page de l'application
+        Elle sollicite à son tour les micro interfaces déployées ailleurs par d'autres développeurs, en l'occurence le menu déroulant Sidebar
       </div>
     <Footer />
   </div>
--- packages/home/src/Header.jsx
+++ packages/home/src/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function Header({app}){
+export default function Header(){
     return(
         <div className='bg-green-800 p-5 text-white text-3xl font-bold text-center'>
             SYSTEME INFORMATIQUE  DE GESTION DES ENTITES FORESTIERES - LOGOS (contenu statique)
             <div>Barre d'authentification - Utilisateur connecté - Login/Logout(contenu dynamique)</div>
-            {app.name}
+            
         </div>
         
     )
