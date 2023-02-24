03f3fff824ba72357675398cd3b0f8630ae74998
karoud <pmandel0001@yahoo.fr>
2023-02-24 21:37:04 +0100

    removed suspense for lazyness downloading of components

--- packages/pdp/src/App.jsx
+++ packages/pdp/src/App.jsx
@@ -1,23 +1,17 @@
-import React, { Suspense,useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.scss";
-// import Header from "home/Header";
-const Header=React.lazy(()=>import('home/Header'))
+import Header from "home/Header";
 import Footer from "home/Footer"
 
-const App = () => {
-  const [showHeader,setShowHeader]=useState(false)
-  return  (
-  <div className="text-3xl mx-auto max-w-6xl">
-    {showHeader && <Suspense fallback={<div>Loading...</div>}>
-        <Header />
-    </Suspense>}
-    <button className="text-3xl p-5" onClick={()=>setShowHeader(true)}>Show the Header</button>
+const App = () => (
+  <div className="mt-10 text-3xl mx-auto max-w-6xl">
+    <Header />
     <div className="my-10">
         PDP page Content
       </div>
     <Footer />
   </div>
-)};
+);
 ReactDOM.render(<App />, document.getElementById("app"));
