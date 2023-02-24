95e350230ec5a4886aec5f53e39217fbc4e54433
karoud <pmandel0001@yahoo.fr>
2023-02-24 19:42:50 +0100

    addded header and footer by the Home Team

---
+++ packages/home/src/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer(){
+    return(
+        <div className='p-5 bg-blue-500 font-bold text-3xl'>
+            Only the best spinners
+        </div>
+    )
+}
---
+++ packages/home/src/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header(){
+    return(
+        <div className='bg-blue-500 p-5 text-white text-3xl font-bold'>
+            Fidget Spinner World
+        </div>
+    )
+}
