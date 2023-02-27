334aa5740ad2800aa0620a03d6d3a39d83c3a63c
puepi <pmandel0001@yahoo.fr>
2023-02-27 13:04:27 +0100

    added react-router-dom to pdp and added cart authorized module to server

--- packages/pdp/src/App.jsx
+++ packages/pdp/src/App.jsx
@@ -1,24 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 import "./index.scss";
 import Header from "home/Header";
 import Footer from "home/Footer"
 import SafeComponent from "./SafeComponent";
-import { PDPContent } from "./PDPContent";
+import PDPContent from "./PDPContent";
 
 const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <SafeComponent>
+  <Router>
+    <div className="mt-10 text-3xl mx-auto max-w-6xl">
       <Header />
-    </SafeComponent>
-    <div className="my-10">
-        Contenu d'une page principale d'un des modules. Par exemple, la page principale de la sous-page Demandes de permis
-        <div>
-          <PDPContent></PDPContent>
+      <div className="my-10">
+          Contenu d'une page principale d'un des modules. Par exemple, la page principale de la sous-page Demandes de permis
+          <div className="my-10">
+           <Switch>
+              <Route path="/product/:id" component={PDPContent} />
+           </Switch>
+          </div>
         </div>
-      </div>
-    <Footer />
-  </div>
+      <Footer />
+    </div>
+  </Router>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
--- packages/pdp/src/PDPContent.jsx
+++ packages/pdp/src/PDPContent.jsx
@@ -1,8 +1,38 @@
 import React,{useState,useEffect} from 'react'
+import {getProductById,currency} from 'home/products'
+import { useParams } from 'react-router-dom'
 
-export const PDPContent = () => {
-    const product=1
+const PDPContent = () => {
+    const {id}=useParams()
+    const [product,setProduct]=useState(null)
+    useEffect(()=>{
+        if(id){
+            getProductById(id).then((product)=>{
+                setProduct(product  )
+            })
+        }else{
+            setProduct(null)
+        }
+    },[id])
+    if(!product)
+        return null
   return (
-    <div>Product {product}</div>
+    <div className='grid grid-cols-2 gap-5'>
+        <div>
+            <img src={product.image} alt={product.name}/>
+        </div>
+        <div>
+            <div className='flex'>
+                <h1 className='font-bold text-3xl flex-grow'>{product.name}</h1>
+                <div className='font-bold text-3xl flex-end'>
+                    {currency.format(product.price)}
+                </div>
+            </div>
+            <div className='mt-10'>{product.description}</div>
+            <div className='mt-10'>{product.longDescription}</div>
+        </div>
+    </div>
   )
 }
+
+export default PDPContent
