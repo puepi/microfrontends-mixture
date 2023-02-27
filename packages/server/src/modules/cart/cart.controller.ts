2b76ae0442d005d3580670ba001b1b05840ee6cb
puepi <pmandel0001@yahoo.fr>
2023-02-27 13:36:23 +0100

    modified cart controller on the server

--- packages/server/src/modules/cart/cart.controller.ts
+++ packages/server/src/modules/cart/cart.controller.ts
@@ -4,15 +4,39 @@ import {
   Request,
   UseGuards,
 } from '@nestjs/common';
+// import { Product } from 'src/products';
 import { JwtAuthGuard } from '../../auth/jwt-auth.guard';
+import products,{Product} from '../../products'
+
+interface CartItem extends Product{
+  quantity:number
+}
+
+interface Cart{
+  cartItems:CartItem[]
+}
+
+const initialCart=(indexes:number[]):Cart=>({
+  cartItems:indexes.map(index=>(
+    {
+      ...products[index],
+      quantity:1
+    }
+  ))
+})
 
 @Controller('cart')
 export class CartController {
+  private carts:Record<number,Cart>={
+    1:initialCart([0,2,4]),
+    2:initialCart([1,3]),
+  }
   constructor() {}
 
   @Get()
   @UseGuards(JwtAuthGuard)
-  async index(@Request() req): Promise<{userId: number}> {
-    return { userId: req.user.userId };
+  async index(@Request() req): Promise<Cart> {
+    // return { userId: req.user.userId };
+    return this.carts[req.user.userId] ?? {cartItems:[]}
   }
 }
